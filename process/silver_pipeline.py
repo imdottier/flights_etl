6efb605f75729a48201515bed7381603f9378098
imdottier <minidotty2309@gmail.com>
2025-10-10 21:00:39 +0000

    minor code refactor, mainly for code transferring

---
+++ process/silver_pipeline.py
@@ -0,0 +1,40 @@
+from pyspark.sql import SparkSession, DataFrame
+from utils.spark_session import get_spark_session
+from utils.logging_utils import setup_logging
+from process.utils import json_to_df, write_delta_table
+
+
+if __name__ == "__main__":
+    log_filepath = setup_logging(log_name="spark_run")
+    spark: SparkSession = get_spark_session()
+
+    flights_df = json_to_df(
+        spark=spark,
+        table_name="flights", 
+        schema=flights_schema
+    )
+
+    airports_df = json_to_df(
+        spark=spark,
+        table_name="airports", 
+        schema=full_airport_schema
+    )
+
+    spark.sql("DROP DATABASE IF EXISTS bronze CASCADE")
+
+    spark.sql("CREATE DATABASE IF NOT EXISTS bronze")
+
+    write_delta_table(
+        spark=spark,
+        df=flights_df,
+        db_name="bronze",
+        table_name="flights",
+        partition_cols=["ingestion_hour"]
+    )
+
+    write_delta_table(
+        spark=spark,
+        df=airports_df,
+        db_name="bronze",
+        table_name="airports",
+    )
--- process/utils.py
+++ process/utils.py
@@ -2,8 +2,16 @@ import os
 import logging
 from pathlib import Path
 
+import re
+import pandas as pd
+
 from pyspark.sql import SparkSession, DataFrame
-from pyspark.sql.types import StructType
+from pyspark.sql.functions import (
+    pandas_udf, col, coalesce, to_timestamp, explode,
+    sha2, concat_ws, current_timestamp, xxhash64, lit
+)
+import functools
+from pyspark.sql.types import StructType,  StringType
 from delta.tables import DeltaTable
 
 from dotenv import load_dotenv
@@ -56,6 +64,81 @@ def json_to_df(
     except Exception as e:
         logging.error(f"Failed to read JSON from path '{path_to_read}'. Error: {e}", exc_info=True)
         raise
+
+
+def camel_to_snake(name: str) -> str:
+    """Converts a single string from camelCase to snake_case."""
+    if name is None:
+        return None
+    return re.sub(r'(?<!^)(?=[A-Z])', '_', name).lower()
+
+
+def flatten_df(df: DataFrame, cols_to_snake_case_values: set[str] | None = None) -> DataFrame:
+    if cols_to_snake_case_values is None:
+        cols_to_snake_case_values = set()
+
+    @pandas_udf(StringType())
+    def camel_to_snake_vectorized(series: pd.Series) -> pd.Series:
+        return series.str.replace(r'(?<!^)(?=[A-Z])', '_', regex=True).str.lower()
+    
+    flat_cols = []
+
+    def get_flat_cols(schema: StructType, prefix: str = ""):
+        for field in schema.fields:
+            full_col_name = f"{prefix}.{field.name}" if prefix else field.name
+
+            if isinstance(field.dataType, StructType):
+                get_flat_cols(field.dataType, prefix=f"{full_col_name}")
+            else:
+                alias_name = camel_to_snake(full_col_name.replace(".", "_"))
+
+                if alias_name in cols_to_snake_case_values and isinstance(field.dataType, StringType):
+                    flat_cols.append(
+                        camel_to_snake_vectorized(col(full_col_name)).alias(alias_name)
+                    )
+                else:
+                    flat_cols.append(
+                        col(full_col_name).alias(alias_name)
+                    )
+
+    try:
+        get_flat_cols(df.schema)
+        
+        if not flat_cols:
+            logging.warning("flatten_df resulted in no columns. Returning an empty DataFrame.")
+            return df.sparkSession.createDataFrame([], schema=StructType([]))
+            
+        return df.select(flat_cols)
+
+    except Exception as e:
+        logging.error("An error occurred during DataFrame flattening.", exc_info=True)
+        raise
+
+
+def transform_timestamps(df: DataFrame) -> DataFrame:
+    source_format = "yyyy-MM-dd HH:mm'Z'"
+    iso_8601_format = "yyyy-MM-dd'T'HH:mm:ss'Z'"
+    local_with_offset_format = "yyyy-MM-dd HH:mmXXX"
+
+    utc_cols = [c for c in df.columns if c.endswith("_utc")]
+    local_cols = [c for c in df.columns if c.endswith("_local")]
+
+    for utc_col in utc_cols:
+        df = df.withColumn(
+            utc_col,
+            coalesce(
+                to_timestamp(col(utc_col), source_format),
+                to_timestamp(col(utc_col), iso_8601_format),
+            )
+        )
+
+    for local_col in local_cols:
+        df = df.withColumn(
+            local_col + "_ntz",
+            to_timestamp(col(local_col), local_with_offset_format)
+        )
+
+    return df
 
 
 def write_delta_table(
@@ -134,3 +217,142 @@ def write_delta_table(
     except Exception as e:
         logging.error(f"FATAL: Failed during WRITE phase for '{full_table_name}'. Error: {e}", exc_info=True)
         raise
+
+
+def write_delta_table_with_scd(
+    spark: SparkSession,
+    df: DataFrame,
+    db_name: str,
+    table_name: str,
+    business_keys: list[str],
+    surrogate_key: str,
+):
+    full_table_name = f"{db_name}.{table_name}"
+    table_path = f"/home/dottier/flights_etl/{db_name}/{table_name}"
+
+    if not business_keys or surrogate_key is None:
+        raise ValueError("Business keys and surrogate key must be provided for SCD Type 2 merge.")
+
+    logging.info(f"--- Preparing to write to Delta table: {full_table_name} ---")
+
+    now = current_timestamp()
+
+    try:
+        if not spark.catalog.tableExists(full_table_name):
+            logging.info(f"Table {full_table_name} does not exist. Creating new Delta table.")
+
+            df = df.withColumn(
+                "effective_start_date", now
+            ).withColumn(
+                "effective_end_date", lit(None).cast("timestamp")
+            )
+
+            df = df.select(
+                xxhash64(concat_ws("|", *business_keys, now)).alias(surrogate_key),
+                *df.columns,
+            )
+            
+            df.write.format("delta").mode("overwrite").option("mergeSchema", "true").save(table_path)
+
+            logging.info(f"Registering Delta table {full_table_name} at location {table_path}.")
+            spark.sql(f"CREATE TABLE {full_table_name} USING DELTA LOCATION '{table_path}'")
+            logging.info(f"Table {full_table_name} created successfully.")
+
+            return
+        
+        logging.info(f"Table {full_table_name} exists. Performing SCD Type 2 merge.")
+        target_table = DeltaTable.forName(spark, full_table_name)
+
+        non_key_columns = [col for col in df.columns if col not in business_keys]
+        source_df_with_hash = df.withColumn(
+            "row_hash",
+            sha2(concat_ws("|", *non_key_columns), 256)
+        )
+
+        source_df_with_hash.printSchema()
+
+        target_df_with_hash = (
+            target_table.toDF()
+            .filter(col("effective_end_date").isNull())
+            .withColumn(
+                "row_hash",
+                sha2(concat_ws("|", *non_key_columns), 256)
+            )
+        )
+
+        target_df_with_hash.printSchema()
+
+        join_condition = [
+            col(f"source.{key}") == col(f"target.{key}") for key in business_keys
+        ]
+        final_join_condition = functools.reduce(lambda x, y: x & y, join_condition) if join_condition else None
+
+        staging_df = source_df_with_hash.alias("source").join(
+            target_df_with_hash.alias("target"),
+            on=final_join_condition,
+            how="left"
+        ).select(
+            "source.*",
+            col(f"target.{surrogate_key}").alias("existing_sk"),
+            col("target.row_hash").alias("existing_hash")
+        )
+
+        expired_records = (
+            staging_df
+            .filter((col("existing_sk").isNotNull()) & (col("existing_hash") != col("row_hash")))
+            .select(
+                col("existing_sk").alias(surrogate_key),
+                now.alias("effective_end_date")
+            )
+        )
+
+        new_records = (
+            staging_df
+            .select(
+                xxhash64(concat_ws("||", *business_keys, now)).alias(surrogate_key),
+                *df.columns,
+                now.alias("effective_start_date"),
+                lit(None).cast("timestamp").alias("effective_end_date")
+            )
+        )
+
+        final_change_set = new_records.unionByName(expired_records, allowMissingColumns=True)
+
+        (
+            target_table.alias("target")
+            .merge(
+                final_change_set.alias("source"),
+                condition=f"target.{surrogate_key} = source.{surrogate_key}"
+            )
+            .whenNotMatchedInsertAll()
+            .whenMatchedUpdate(
+                set={"effective_end_date": "source.effective_end_date"}
+            )
+            .execute()
+        )
+
+        logging.info(f"SCD Type 2 merge into {full_table_name} completed successfully.")
+
+    except Exception as e:
+        logging.error(f"FATAL: Failed during WRITE phase for '{full_table_name}'. Error: {e}", exc_info=True)
+        raise
+
+
+def enrich_flights_data(
+    bronze_flights: DataFrame,
+):
+    departure_exploded_df = (
+        bronze_flights.select(
+            explode("departures").alias("data"),
+            "airport",
+            "ingestion_hour",
+        )
+    )
+
+    arrival_exploded_df = (
+        bronze_flights.select(
+            explode("arrivals").alias("data"),
+            "airport",
+            "ingestion_hour",
+        )
+    )
